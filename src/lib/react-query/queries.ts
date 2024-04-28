0d7977046ac2925d479318baaf0d8f677bf78177
Paweł <pawel_gnat@o2.pl>
2024-04-28 16:27:31 +0200

    feat: create post

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -1,48 +1,114 @@
+import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
+
+import { QUERY_KEYS } from '@/lib/react-query/queryKeys'
 import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    useInfiniteQuery,
-  } from "@tanstack/react-query";
+	createUserAccount,
+	signInAccount,
+	// getCurrentUser,
+	signOutAccount,
+	// getUsers,
+	createPost,
+	// getPostById,
+	updatePost,
+	// getUserPosts,
+	// deletePost,
+	// likePost,
+	// getUserById,
+	// updateUser,
+	getRecentPosts,
+	// getInfinitePosts,
+	// searchPosts,
+	// savePost,
+	// deleteSavedPost,
+} from '@/lib/appwrite/api'
+import { INewPost, INewUser, IUpdatePost, IUpdateUser } from '@/types'
 
-//   import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
-  import {
-    createUserAccount,
-    signInAccount,
-    // getCurrentUser,
-    signOutAccount,
-    // getUsers,
-    // createPost,
-    // getPostById,
-    // updatePost,
-    // getUserPosts,
-    // deletePost,
-    // likePost,
-    // getUserById,
-    // updateUser,
-    // getRecentPosts,
-    // getInfinitePosts,
-    // searchPosts,
-    // savePost,
-    // deleteSavedPost,
-  } from "@/lib/appwrite/api";
-  import { INewPost, INewUser, IUpdatePost, IUpdateUser } from "@/types";
+export const useCreateUserAccount = () => {
+	return useMutation({
+		mutationFn: (user: INewUser) => createUserAccount(user),
+	})
+}
 
-  export const useCreateUserAccount = () => {
-    return useMutation({
-      mutationFn: (user: INewUser) => createUserAccount(user),
-    });
-  };
-  
-  export const useSignInAccount = () => {
-    return useMutation({
-      mutationFn: (user: { email: string; password: string }) =>
-        signInAccount(user),
-    });
-  };
-  
-  export const useSignOutAccount = () => {
-    return useMutation({
-      mutationFn: signOutAccount,
-    });
-  };
+export const useSignInAccount = () => {
+	return useMutation({
+		mutationFn: (user: { email: string; password: string }) => signInAccount(user),
+	})
+}
+
+export const useSignOutAccount = () => {
+	return useMutation({
+		mutationFn: signOutAccount,
+	})
+}
+
+export const useCreatePost = () => {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: (post: INewPost) => createPost(post),
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+			})
+		},
+	})
+}
+
+export const useUpdatePost = () => {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: (post: IUpdatePost) => updatePost(post),
+		onSuccess: data => {
+			queryClient.invalidateQueries({
+				queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+			})
+		},
+	})
+}
+
+export const useGetRecentPosts = () => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+		queryFn: getRecentPosts,
+	})
+}
+
+export const useGetPosts = () => {
+	return useInfiniteQuery({
+		queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+		queryFn: getInfinitePosts as any,
+		getNextPageParam: (lastPage: any) => {
+			// If there's no data, there are no more pages.
+			if (lastPage && lastPage.documents.length === 0) {
+				return null
+			}
+
+			// Use the $id of the last document as the cursor.
+			const lastId = lastPage.documents[lastPage.documents.length - 1].$id
+			return lastId
+		},
+	})
+}
+
+export const useSearchPosts = (searchTerm: string) => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+		queryFn: () => searchPosts(searchTerm),
+		enabled: !!searchTerm,
+	})
+}
+
+export const useGetPostById = (postId?: string) => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+		queryFn: () => getPostById(postId),
+		enabled: !!postId,
+	})
+}
+
+export const useGetUserPosts = (userId?: string) => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.GET_USER_POSTS, userId],
+		queryFn: () => getUserPosts(userId),
+		enabled: !!userId,
+	})
+}
